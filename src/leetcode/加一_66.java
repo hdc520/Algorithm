e2b78e51bf38a7fb2c60bc5251ea61fee6eb1612
hdc520 <1712861889@qq.com>
2020-08-10 14:19:44 +0800

    leetcode上的第66题，数组表示的数加1

--- src/leetcode/加一_66.java
+++ src/leetcode/加一_66.java
@@ -1,4 +1,23 @@
 package leetcode;
 
 public class 加一_66 {
+    public static void main(String[] args) {
+        int nums[]=new int[]{9,9,9,9,9,9};
+        for(int i:plusOne(nums)){
+            System.out.print(i+" ");
+        }
+    }
+
+    public static int[] plusOne(int nums[]){
+        for(int i=nums.length-1;i>=0;i--){
+            nums[i]++;
+            if(nums[i]<10){
+                return nums;
+            }
+            nums[i]=nums[i]%10;
+        }
+        int ans[]=new int[nums.length+1];
+        ans[0]=1;
+        return ans;
+    }
 }
