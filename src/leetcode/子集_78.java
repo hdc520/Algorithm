32d735bf3efb5eb5666fa7552dbfb3e0ff61cb93
hdc520 <1712861889@qq.com>
2020-08-11 11:29:47 +0800

    leetcode第78题，回溯法求解子集

---
+++ src/leetcode/子集_78.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 子集_78 {
+    static List<List<Integer>> ans=new ArrayList<>();
+    public static void main(String[] args) {
+        subsets(new int[]{1,2,3});
+        for(List l:ans){
+            for(Object i:l){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<List<Integer>> subsets(int[] nums){
+        List<Integer> track=new ArrayList<>();
+//        ans.add(track);
+        for(int i=0;i<nums.length+1;i++){
+            backTrack(nums,track,i,0);
+        }
+        return ans;
+    }
+
+    public static void backTrack(int nums[],List<Integer>track ,int k,int cur){
+        if(track.size()==k){
+            ans.add(new ArrayList<>(track));
+            return;
+        }
+        for(int i=cur;i<nums.length;i++){
+            if(track.contains(nums[i])){
+                continue;
+            }
+            track.add(nums[i]);
+            backTrack(nums, track, k, i+1);
+            track.remove(track.size()-1);
+        }
+    }
+}
