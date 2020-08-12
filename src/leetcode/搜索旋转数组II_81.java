b7ed36862ca869572b65c28a6ba1c0eac8d2d31c
hdc520 <1712861889@qq.com>
2020-08-12 17:24:24 +0800

    leetcode第81题，有序重复数组旋转后利用二分法查找

---
+++ src/leetcode/搜索旋转数组II_81.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class 搜索旋转数组II_81 {
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{2,2,2,2,2,5,6,2,2,2},5));
+    }
+    public static boolean search(int nums[],int target){
+        int left=0,right=nums.length-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return true;
+            }else if(nums[mid]<nums[right]){
+                if(nums[mid]<target&&target<=nums[right]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }else if(nums[mid]>nums[right]){
+                if(nums[mid]>target&&nums[left]<=target){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }else{
+                while(left<=right&&nums[mid]==nums[left]){
+                    left++;
+                }
+                while(left<=right&&nums[mid]==nums[right]){
+                    right--;
+                }
+            }
+        }
+        return false;
+    }
+}
