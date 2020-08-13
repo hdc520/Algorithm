6f5e347e42933e0e5975a88de93296302c974f5e
hdc520 <1712861889@qq.com>
2020-08-13 16:15:18 +0800

    leetcode第84题，求最大矩形面积，注意代码11行和13行求最小值的方法

---
+++ src/leetcode/柱状图中最大的矩形面积_84.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class 柱状图中最大的矩形面积_84 {
+    public static void main(String[] args) {
+        System.out.println(largestRectangleArea(new int[]{2,1,5,6,2,3}));
+    }
+    public static int largestRectangleArea(int[] heights) {
+        int min=Integer.MAX_VALUE;
+        int ans=0;
+        for(int i=0;i<heights.length;i++){
+            min=Integer.MAX_VALUE;
+            for(int j=i;j<heights.length;j++){
+                min=Math.min(min,heights[j]);
+                ans=Math.max(ans,(j-i+1)*min);
+            }
+        }
+        return ans;
+    }
+}
