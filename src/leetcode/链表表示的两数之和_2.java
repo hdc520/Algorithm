026e9b8b781c5ae2a8907c22b8d0ed1a3e13c945
hdc520 <1712861889@qq.com>
2020-09-02 20:39:34 +0800

    链表表示的两数之和

---
+++ src/leetcode/链表表示的两数之和_2.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import comAlgo.list.ListNode;
+
+public class 链表表示的两数之和_2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head=new ListNode(-1);
+        ListNode r=head;
+        int carry=0;
+        while(l1!=null||l2!=null||carry!=0){
+            if(l1!=null){
+                carry+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                carry+=l2.val;
+                l2=l2.next;
+            }
+            ListNode p=new ListNode(carry%10);
+            carry=carry/10;
+            r.next=p;
+            r=p;
+        }
+        return head.next;
+    }
+}
