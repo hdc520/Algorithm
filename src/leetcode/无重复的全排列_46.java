3f425c35d7ceef6a7df7f9286c26a64bd51127d3
hdc520 <1712861889@qq.com>
2020-09-02 11:15:04 +0800

    利用list的contains和remove方法回溯法求全排列

---
+++ src/leetcode/无重复的全排列_46.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 无重复的全排列_46 {
+    public static void main(String[] args) {
+        List<List<Integer>> ans=premute(new int[]{2,3,4});
+        for(List l:ans){
+            for(Object i:l){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<List<Integer>> premute(int nums[]){
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> track=new ArrayList<>();
+        backTrack(ans,track,nums);
+        return ans;
+    }
+    public static void backTrack(List<List<Integer>> ans,List<Integer> track,int nums[]){
+        if(track.size()==nums.length){
+            ans.add(new ArrayList<>(track));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(track.contains(nums[i])){
+                continue;
+            }
+            track.add(nums[i]);
+            backTrack(ans,track,nums);
+            track.remove(track.size()-1);
+        }
+    }
+}
