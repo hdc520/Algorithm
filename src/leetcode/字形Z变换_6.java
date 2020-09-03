1337136a811829277c25785fe2cc2e5b1d2ed404
hdc520 <1712861889@qq.com>
2020-09-03 10:22:46 +0800

    一维数组的反复横跳以及推广到二维数组的反复横跳

---
+++ src/leetcode/字形Z变换_6.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 字形Z变换_6 {
+    public String convert(String s, int numRows) {
+        if(numRows==1){
+            return s;
+        }
+        List<StringBuilder> list=new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            list.add(new StringBuilder());
+        }
+        int curRow=0,flag=1;
+        for(int i=0;i<s.length();i++){
+            list.get(curRow).append(s.charAt(i));
+            if(curRow==numRows-1){
+                flag=-1;
+            }
+            if(curRow==0){
+                flag=1;
+            }
+            curRow+=flag;
+        }
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<numRows;i++){
+            System.out.println(list.get(i).toString());
+            ans.append(list.get(i).toString());
+        }
+        return ans.toString();
+    }
+}
