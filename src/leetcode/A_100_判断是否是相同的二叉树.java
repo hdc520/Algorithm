b95bc7a61487d290a9ad34180747dcca9492aa23
hdc520 <1712861889@qq.com>
2020-09-04 11:33:09 +0800

    递归判断两个树是否是相等的树

--- src/leetcode/A_100_判断是否是相同的二叉树.java
+++ src/leetcode/A_100_判断是否是相同的二叉树.java
@@ -2,7 +2,7 @@ package leetcode;
 
 import comAlgo.tree.TreeNode;
 
-public class 判断是否是相同的二叉树 {
+public class A_100_判断是否是相同的二叉树 {
     public boolean isSameTree(TreeNode p,TreeNode q){
         if(p==null&&q==null){
             return true;
