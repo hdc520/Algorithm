04be3efa2cc81f0203dd97ab1f51218032b31b44
hdc520 <1712861889@qq.com>
2020-09-04 11:38:05 +0800

    重复数字的数组求子集，注意33行去重的代码

--- src/leetcode/A_90_子集II.java
+++ src/leetcode/A_90_子集II.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class 子集II_90 {
+public class A_90_子集II {
     public static List<List<Integer>> ans;
     public static void main(String[] args) {
         subsetsWithDup(new int[]{1,2,2});
