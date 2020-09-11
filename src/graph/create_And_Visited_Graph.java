14b36e4d77c4987d2b7a9811a6013152a79313a3
hdc520 <1712861889@qq.com>
2020-09-11 10:54:58 +0800

    图

--- src/graph/create_And_Visited_Graph.java
+++ src/graph/create_And_Visited_Graph.java
@@ -20,23 +20,15 @@ public class create_And_Visited_Graph {
         graphNode n5=new graphNode(5);
 
         //创建邻接表
-        n1.neighbors.add(n2);
-        n1.neighbors.add(n5);
+        n1.neighbors.add(n2);n1.neighbors.add(n5);
 
-        n2.neighbors.add(n1);
-        n2.neighbors.add(n5);
-        n2.neighbors.add(n4);
+        n2.neighbors.add(n1);n2.neighbors.add(n5);n2.neighbors.add(n4);
 
-        n3.neighbors.add(n2);
-        n3.neighbors.add(n4);
+        n3.neighbors.add(n2);n3.neighbors.add(n4);
 
-        n4.neighbors.add(n2);
-        n4.neighbors.add(n5);
-        n4.neighbors.add(n3);
+        n4.neighbors.add(n2);n4.neighbors.add(n5);n4.neighbors.add(n3);
 
-        n5.neighbors.add(n4);
-        n5.neighbors.add(n1);
-        n5.neighbors.add(n2);
+        n5.neighbors.add(n4);n5.neighbors.add(n1);n5.neighbors.add(n2);
         //深度优先遍历
         DFS(n1);
 //        System.out.println();
