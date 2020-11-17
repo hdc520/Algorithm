51143c1cb079ef513200eaa551956b048fe8e0ff
hdc520 <1712861889@qq.com>
2020-11-17 16:16:47 +0800

    求TopN

--- src/comAlgo/sort/Top2.java
+++ src/comAlgo/sort/Top2.java
@@ -2,11 +2,13 @@ package comAlgo.sort;
 
 public class Top2 {
     public static void main(String[] args) {
-        int nums[]=new int[]{1,9,5,3,4,2,6,8,7};
+        int nums[]=new int[]{1,3,5,8,9,6,7,12};
         int max=0,secondMax=0;
         for(int i:nums){
             if(i>=max){
+                int temp=max;
                 max=i;
+                secondMax=temp;
             }else{
                 if(i>=secondMax){
                     secondMax=i;
