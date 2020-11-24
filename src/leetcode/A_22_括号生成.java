6a678619a81ed916d542ea3ad8eeb6e9ebeb504b
hdc520 <1712861889@qq.com>
2020-11-24 14:26:53 +0800

    括号生成

--- src/leetcode/A_22_括号生成.java
+++ src/leetcode/A_22_括号生成.java
@@ -29,3 +29,28 @@ public class A_22_括号生成 {
         }
     }
 }
+class Solution1 {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        if (n == 0) {
+            return res;
+        }
+        dfs("", n, n, res);
+        return res;
+    }
+    private void dfs(String curStr, int left, int right, List<String> res) {
+        if (left == 0 && right == 0) {
+            res.add(curStr);
+            return;
+        }
+        if (left > right) {
+            return;
+        }
+        if (left > 0) {
+            dfs(curStr + "(", left - 1, right, res);
+        }
+        if (right > 0) {
+            dfs(curStr + ")", left, right - 1, res);
+        }
+    }
+}
